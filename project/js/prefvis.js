3335b2106f5bf079b5f584a88cfe1ca525358b1a
dtan44 <dylantan@college.harvard.edu>
2015-05-05 20:31:29 -0400

    bordering

--- project/js/prefvis.js
+++ project/js/prefvis.js
@@ -7,8 +7,10 @@ PrefVis = function(_parentElement, _data, _selectionElement){
         wave: null
     };
 
+    this.w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+    this.h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
     this.margin = {top: 50, right: 0, bottom: 100, left: 80},
-    this.width = 700 - this.margin.left - this.margin.right,
+    this.width = this.w*.4 - this.margin.left - this.margin.right,
     this.height = 500 - this.margin.top - this.margin.bottom;
 
     this.initVis();
